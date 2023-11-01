dd29031247a0cafdc4e9134dec3d3ca81140f5ac
strbagus <satrio.baguswbw@gmail.com>
2023-11-01 21:23:44 +0700

    Route, Handlers, Models, Middleware, etc

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/strbagus/rssagg/internal/database"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 
@@ -16,6 +24,19 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("Database is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("can't connect to database", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -29,18 +50,21 @@ func main() {
 		MaxAge:           300,
 	}))
 
-  v1Router := chi.NewRouter()
-  v1Router.Get("/healthz", handlerReadiness)
-  v1Router.Get("/err", handlerErr)
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+  v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+  v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 
-  router.Mount("/v1", v1Router)
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
 	}
 	log.Printf("Server Starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ models.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/strbagus/rssagg/internal/database"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	APIKey    string    `json:"api_key"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+		APIKey:    dbUser.ApiKey,
+	}
+}
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	URL       string    `json:"url"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func databaseFeedToFeed(dbFeed database.Feed) Feed {
+	return Feed{
+		ID:        dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name:      dbFeed.Name,
+		URL:       dbFeed.Url,
+		UserID:    dbFeed.UserID,
+	}
+}
